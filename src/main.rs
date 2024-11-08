cfef0b4457a963b8798530e14f0761aee078cce1
dora-explora <sarrafdara@gmail.com>
2024-11-08 15:35:15 -0800

    started with reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let path = &env::args().collect::<Vec<_>>()[1]; // set filepath to first arg
+    let contents = fs::read_to_string(path) // read file
+        .expect("File could not be read successfully");
+
+    println!("{}", contents);
 }

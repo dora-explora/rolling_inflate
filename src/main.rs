42a47457490b305550ccb8e006554bee970c0214
dora-explora <sarrafdara@gmail.com>
2024-11-09 23:52:27 -0800

    file is read better now

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use std::env;
+// use std::env;
 use std::fs;
+use std::io::Read;
 
 fn main() {
-    let path = &env::args().collect::<Vec<_>>()[1]; // set filepath to first arg
-    let contents = fs::read_to_string(path) // read file
-        .expect("File could not be read successfully");
+    // let path = &env::args().collect::<Vec<_>>()[1]; // set filepath to first arg
+    let path = "hello.txt";
+    let mut file = fs::File::open(path) // read file
+        .expect("File could not be opened successfully");
+    const BUFFER_LENGTH: usize = 32;
+    let mut buffer = [0; BUFFER_LENGTH];
+    let n  = file.read(&mut buffer).expect("File could not be read successfully");
+    let mut eof = false;
+    if n != BUFFER_LENGTH {
+        eof = true;
+    }
 
-    println!("{}", contents);
+    println!("{}", buffer[0]);
 }

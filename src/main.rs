b0d6f61a6f7712a44fd86b2acb64608cd79b876b
DoraExplarora <114842453+DoraExplarora@users.noreply.github.com>
2024-11-10 17:16:56 -0800

    gzip file header is being read!!

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,84 @@
 // use std::env;
-use std::fs;
+use std::fs::File;
 use std::io::Read;
+use bitvec::prelude::*;
+
+fn read_bytes(mut cursor: u64, buffer_length: usize, mut file: &mut File, mut eof: bool) -> Vec<u8> {
+    let mut buffer = vec![0u8; buffer_length];
+    let n  = file.read(&mut buffer).expect("File could not be read successfully"); // reads part of file into buffer
+    cursor += n as u64;
+    if n != buffer_length {
+        eof = true; // this would only happen if the entire buffer is not filled
+    }
+    return buffer;
+}
+
+fn read_bits(mut cursor: u64, buffer_length: usize, mut file: &mut File, mut eof: bool) -> BitVec<u8, Lsb0> {
+    let mut buffer = vec![0u8; buffer_length];
+    let n  = file.read(&mut buffer).expect("File could not be read successfully"); // reads part of file into buffer
+    cursor += n as u64;
+    if n != buffer_length {
+        eof = true; // this would only happen if the entire buffer is not filled
+    }
+    let bits: BitVec<u8, Lsb0> = BitVec::from_slice(&buffer);
+    return bits;
+}
 
 fn main() {
     // let path = &env::args().collect::<Vec<_>>()[1]; // set filepath to first arg
-    let path = "hello.txt";
-    let mut file = fs::File::open(path) // read file
+    let path = "hello.txt.gz";
+    let mut file = File::open(path) // open file
         .expect("File could not be opened successfully");
-    const BUFFER_LENGTH: usize = 32;
-    let mut buffer = [0; BUFFER_LENGTH];
-    let n  = file.read(&mut buffer).expect("File could not be read successfully");
-    let mut eof = false;
-    if n != BUFFER_LENGTH {
-        eof = true;
+    let eof = false; // true if encountered the end of the file
+    let cursor: u64 = 0;
+    let mut buffer: Vec<u8>;
+    let mut bits: BitVec<u8, Lsb0>;
+    buffer = read_bytes(cursor, 3, &mut file, eof);
+
+    if buffer[0] != 0x1F || buffer[1] != 0x8B {
+        panic!("File is not a gzip file")
+    } else if buffer[2] != 8 {
+        panic!("File is not compressed with deflate")
     }
 
-    println!("{}", buffer[0]);
+    bits = read_bits(cursor, 1, &mut file, eof);
+    let ftext = bits[0];
+    let fhcrc = bits[1];
+    let fextra = bits[2];
+    let fname = bits[3];
+    let fcomment = bits[4];
+    println!("fcomment = {fcomment}\nfname = {fname}\nfextra = {fextra}\nfhcrc = {fhcrc}\nftext = {ftext}");
+
+    _ = read_bytes(cursor, 6, &mut file, eof); // yeah i dont know what to do with these bytes ._.
+
+    if fextra {
+        buffer = read_bytes(cursor, 2, &mut file, eof);
+        let xlen: u16 = buffer[1] as u16 | ((buffer[2] as u16) << 8);
+        _ = read_bytes(cursor, xlen as usize, &mut file, eof);
+        // i dont know what to do with these either ._.
+    }
+    if fname {
+        let mut name_bytes: Vec<u8> = Vec::new();
+        buffer = read_bytes(cursor, 1, &mut file, eof);
+        while buffer[0] != 0x00 {
+            name_bytes.push(buffer[0]);
+            buffer = read_bytes(cursor, 1, &mut file, eof);
+        }
+        let name = String::from_utf8(name_bytes).unwrap();
+        println!("name: {}", name);
+    }
+    if fcomment {
+        let mut comment_bytes: Vec<u8> = Vec::new();
+        buffer = read_bytes(cursor, 1, &mut file, eof);
+        while buffer[0] != 0x00 {
+            comment_bytes.push(buffer[0]);
+            buffer = read_bytes(cursor, 1, &mut file, eof);
+        }
+        let comment = String::from_utf8(comment_bytes).unwrap();
+        println!("comment: {}", comment);
+    }
+    if fhcrc {
+        _ = read_bytes(cursor, 2, &mut file, eof);
+        // uhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh ._.
+    }
 }

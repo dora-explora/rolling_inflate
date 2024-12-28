df38f8e8d9ec24f68b6cd9de610a3b4b1beb638c
dora-explora <sarrafdara@gmail.com>
2024-12-27 17:56:02 -0800

    I F**KING HATE WINDOWS ISTCD the **ONLY** reason this hasn't been working is because winrar just decided to be horrible at compressing ig AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     // let path = &env::args().collect::<Vec<_>>()[1]; // set filepath to first arg
-    let path = "hello.txt.gz";
+    let path = "lipsum.txt.gz";
     rolling_inflate::run(path);
 }
